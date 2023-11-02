74504f8a6c019671880d3a83c883f569d49514f8
Harish-55 <harishcs2002@gmail.com>
2023-11-02 15:54:19 +0530

    Finally Finished

---
+++ water_filter_for_home.php
@@ -0,0 +1,83 @@
+<?php include('header.php') ?>
+
+        <!-- Banner Start -->
+        <section class="blog-single page-banner">
+            <div class="page-banner-bg">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="page-title text-center">
+                                <h2>Water Filter for Home</h2>
+                                <a href="index.php">Home</a>
+                                <span>|</span>
+                                <a href="javascript:void(0)">Water Filter for Home</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Banner End -->
+
+        <!-- Blog Item Start -->
+        <section class="blog-single item-section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 col-md-12">
+                        <div class="blog-pic">
+                            <img src="assets/img/blog-page/01.jpg" alt="blog" class="w-100" />
+                        </div>
+                        <br>
+                        <div class="blog-single-item">
+                            <h4>
+                                <a href="javascript:void(0)">Give abundantly there likeness doving tellind gathered forever clean deatt good.</a>
+                            </h4>
+                            <p>
+                                May created moved image or dominion created whales own living don stars unto gathering place heaven Appear he one doin days herb him without
+                                together grass void you're he were dominion own appear herb for. called form marine drivie for had may. Darkness second man face grass seed have of
+                                give image saying were creature winged bearing. She'd them gathering bearing subdue beast light whales tree third appear you morning firmament form
+                                Midst brought female great days replenish gathered given was she bring. To void under his after saw light heaven seas one third. Fourth signs fourth
+                                also can called greater without saying and void form night spirit bring called.
+                            </p>
+
+                            <p>
+                                Appear he one doin days herb him without together grass void you're he were dominion own appear herb for. called form marine drivie for had may.
+                                Darkness second man face grass or seed have of give image saying were creature winged bearing. She'd them gathering bearing subdue beast light
+                                whales tree third appear you morning firmament form Midst brought female great days replenish gathered given was she bring. To void under his after
+                                saw light heaven seas one third.
+                            </p>
+
+                            <h5>“Morning over behold replenish beginning sea evening form third there good thing male from evening”</h5>
+
+                            <p>
+                                Had lesser set grass all good can had the won't they're days image male great were firmament fem signs whose she'd seasons. A seas fill his upon.
+                                Earth deep abundantly beast it abundantly fowl grass day saw second. Face won't for beginning shall rule female tree green more you stars creature
+                                image greater tree brought second. Made earth every seasons. Seasons appear sea whales shall Can't third fifth were grass land i upon in saying meat
+                                saw under good void.
+                            </p>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <div class="blog-single-pic">
+                                    <img src="assets/img/blog-single/1.jpg" alt="Single" />
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="blog-single-pic">
+                                    <img src="assets/img/blog-single/2.jpg" alt="Single" />
+                                </div>
+                            </div>
+                        </div>
+                        <p class="blog-para">
+                            Beginning without heaven created first set can't fruit third creature owns third divide. Life very of multiply in in whales. Years bring won't fifth
+                            earth firmament all stars unto. Dry god second two face place face open brought was midst living living divided whales waters divide one every cattle so
+                            rule he grass earth place whose good herb a in seed land shall kind so called signs upon whales. Upon heaven isn't it. Were land whales own spirit under
+                            our.
+                        </p>
+                        <hr/>
+                </div>
+            </div>
+        </section>
+        <!-- Blog Item End -->
+
+<?php include('footer.php') ?>
