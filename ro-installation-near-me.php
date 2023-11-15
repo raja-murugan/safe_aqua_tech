1d4f1e87dc15687af5214cb93ed0aff9435f35d3
Harish-55 <harishcs2002@gmail.com>
2023-11-15 16:47:04 +0530

    Finished

--- ro-installation-near-me.php
+++ ro-installation-near-me.php
@@ -1,9 +1,5 @@
 <?php include('header.php') ?>
-<style>
-ul{
-  list-style-type: circle;
-}
-</style>
+
         <!-- Banner Start -->
         <section class="blog-single page-banner">
             <div class="page-banner-bg">
@@ -27,28 +23,37 @@ ul{
         <section class="blog-single item-section">
             <div class="container">
                 <h2 class="text-left">Our Experts Experience in Words :</h2>
-            
-                <div class="row">
-                    <div class=" col-10 pt-3 ">
-                        <ul class="point">
-                            <li class="point_one">Our installation process is carried out by a team of experienced professionals who have a deep understanding of water purification technology. 
-                                They will ensure that your water purifier is installed correctly and efficiently, minimizing any disruptions to your daily routine.
-                            </li>
-                            <br>
-                            <li>In addition to installation, our team is always ready to provide prompt and reliable service for your water purifier. 
-                                Whether it's routine maintenance, filter replacement, or troubleshooting, we will be there to address any concerns and keep your water purifier functioning at its best.
-                            </li>
-                            <br>
-                            <li>We pride ourselves on offering personalized service and strive to exceed your expectations. 
-                                Our team is trained to provide clear and helpful explanations about your water purifier, ensuring that you have a comprehensive understanding of its features and operation.
-                            </li>
-                            <br>
-                            <li>We believe in the importance of clean and safe drinking water for every household. 
-                                That's why we are committed to delivering excellence in water purifier installation and service, so you can enjoy peace of mind knowing that your water is purified to the highest standards.
-                            </li>
-                        </ul>
-                    </div>  
+                <p>
+                    At Safe Aqua Tech, our team is dedicated to providing excellence in the installation of water purifiers and service. 
+                    With our expertise and commitment to customer satisfaction, you can trust us to deliver the highest quality solutions for your water purification needs.
+                </p>
+                
+                <div class="blog-pic text-center pb-5">
+                    <picture>
+                        <img src="assets/img/ro-installation/1.jpg" alt="blog" class="">
+                    </picture>
                 </div>
+                        <ul>
+                            <p class="pl-5">
+                                <b>1.</b> Our installation process is carried out by a team of experienced professionals who have a deep understanding of water purification technology. 
+                                They will ensure that your water purifier is installed correctly and efficiently, minimizing any disruptions to your daily routine.
+                            </p>
+                            <br>
+                            <p class="pl-5">
+                                <b>2.</b> In addition to installation, our team is always ready to provide prompt and reliable service for your water purifier. 
+                                Whether it's routine maintenance, filter replacement, or troubleshooting, we will be there to address any concerns and keep your water purifier functioning at its best.
+                            </p>
+                            <br>
+                            <p class="pl-5">
+                                <b>3.</b> We pride ourselves on offering personalized service and strive to exceed your expectations. 
+                                Our team is trained to provide clear and helpful explanations about your water purifier, ensuring that you have a comprehensive understanding of its features and operation.
+                            </p>
+                            <br>
+                            <p class="pl-5 pb-5">
+                                <b>4.</b> We believe in the importance of clean and safe drinking water for every household. 
+                                That's why we are committed to delivering excellence in water purifier installation and service, so you can enjoy peace of mind knowing that your water is purified to the highest standards.
+                            </p>
+                        </ul>
             </div>
         </section>
         <!-- Blog Item End -->
---
+++ water-purifiers.php
@@ -0,0 +1,93 @@
+<?php include('header.php') ?>
+
+        <!-- Banner Start -->
+        <section class="blog-single page-banner">
+            <div class="page-banner-bg">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="page-title text-center">
+                                <h2>Water Purifiers</h2>
+                                <a href="index.php">Home</a>
+                                <span>|</span>
+                                <a href="javascript:void(0)">Water Purifiers</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Banner End -->
+
+        <!-- Blog Item Start -->
+        <section class="blog-single item-section">
+            <div class="container">
+                <h2>Importance of Water Purifiers :</h2>
+                <div class="row">
+                    <div class=" col-12 pt-3">
+                        <p>
+                            In a world where the quality of water can directly impact your health, water purifiers have emerged as indispensable guardians of purity. 
+                            Join us as we delve into the significance of water purifiers, understanding why these essential appliances are key to unlocking a healthier, safer lifestyle.
+                        </p>
+                    </div>
+                </div>
+
+                <div class="text-center pb-5">
+                    <picture>
+                        <img src="assets/img/water-purifiers/1.png" alt="blog" class="">
+                    </picture>
+                </div>
+
+                <h5 class="pt-3">Defining Water Purifiers :</h5>
+                <p>
+                    Water purifiers are not mere appliances; they are the unsung heroes in our daily lives. Their primary role is to cleanse water, eliminating impurities and contaminants that can pose risks to our well-being. 
+                    In essence, water purifiers act as gatekeepers, ensuring that what reaches our glasses is nothing short of crystal-clear perfection.
+                </p>
+                
+                <h5 class="pt-3">Ensuring Water Purity :</h5>
+                <p>
+                    The importance of water purifiers lies in their ability to guarantee the purity of the water you consume. 
+                    With advanced filtration technologies, water purifiers remove harmful substances, such as bacteria, viruses, heavy metals, and chemicals, leaving you with water that is not just clear but safe for consumption.    
+                </p>
+                
+                <h5 class="pt-3">Safe Hydration :</h5>
+                <p>
+                    One of the key benefits of water purifiers is their role in providing safe and healthy hydration. 
+                    By ridding water of potential contaminants, water purifiers become the first line of defense against waterborne diseases, ensuring that each sip is a step towards a healthier you. 
+                </p>
+
+                <h5 class="pt-3">Environmental Considerations with Water Purifiers :</h5>
+                <p>
+                    Choosing water purifiers over bottled water contributes to environmental sustainability. 
+                    Water purifiers minimize the need for single-use plastic bottles, reducing plastic waste and lessening the environmental impact. 
+                    It's not just about your health; it's about being mindful stewards of our planet.    
+                </p>
+
+                <h5 class="pt-3">Water Purifiers as Lifestyle Essentials :</h5>
+                <p>
+                    In the modern world, water purifiers have transcended their utilitarian roles. They have become lifestyle essentials, seamlessly integrating into contemporary living spaces. 
+                    With sleek designs and smart functionalities, water purifiers elevate the aesthetics of your surroundings while ensuring the purity of your hydration.  
+                </p>
+
+                <h5 class="pt-3">The Convenience of Water Purifiers :</h5>
+                <p>
+                    In addition to health benefits, water purifiers offer unparalleled convenience. 
+                    Real-time monitoring, filter replacement reminders, and smart connectivity features make water purifiers an integral part of your daily routine, enhancing your overall experience.    
+                </p>
+
+                <h5 class="pt-3">Long-term Health Investment :</h5>
+                <p>
+                    Investing in water purifiers is an investment in long-term health. By safeguarding yourself and your family from waterborne contaminants, you're laying the foundation for a healthier future. 
+                    Water purifiers are not just appliances; they are essential tools for preventive healthcare
+                </p>
+
+                <div class="blog-pic text-center pb-5">
+                    <picture>
+                        <img src="assets/img/water-purifiers/2.jpg" alt="blog" class="">
+                    </picture>
+                </div>
+            </div>
+        </section>
+        <!-- Blog Item End -->
+
+<?php include('footer.php') ?>
